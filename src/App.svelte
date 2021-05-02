987c42e001a3222b97b55f6463f3c53e5490b645
Kevin L <61201328+KL2808@users.noreply.github.com>
2021-05-02 12:10:17 +0200

    Added a nice Header with some hidden links

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
+	import Header from './components/Header.svelte';
+
 	let name = 'NASA-APOD';
 </script>
 
-
-
+<Header/>
 
 <h1>Hello {name}!</h1>
---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 60px;
+        -webkit-box-shadow: 0px 5px 20px 10px rgba(0,0,0,0.2); 
+        box-shadow: 0px 5px 20px 10px rgba(0,0,0,0.2);
+    }
+    .nasa-logo {
+        padding-left: 50px;
+        padding-right: 25px;
+        height: 30px;
+    }
+    .github-logo {
+        height: 30px;
+    }
+    .github-text {
+        padding-left: 25px;
+        padding-right: 50px;
+    }
+</style>
+
+<div class="container">
+    <a href="https:\\www.nasa.gov">
+        <img class="nasa-logo" src="images/nasa-worm.svg" alt="nasa logo worm style"/>
+    </a>
+    <p> APOD <br> Astronomy Picture of the Day </p>
+    <span class="spacer"></span>
+    <a href="https:\\www.github.com\KL2808">
+        <img class="github-logo" src="images/github.svg" alt="github logo"/>
+    </a>
+    <p class="github-text"> Made with ❤️ <br> by KL2808 </p>
+</div>
